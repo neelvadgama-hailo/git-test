bf10691752c7cc001a77e4f1406d19746fcacf29
neelvadgama-hailo <neel@neelvadgama.com>
2015-09-30 11:49:57 +0100

    Initial push

---
+++ helloworld.js
@@ -0,0 +1,22 @@
+export default {
+
+    functionone(a,b) {
+        return a+b;
+    },
+
+    functiontwo(c, d) {
+        return c+d;
+    },
+
+    functionthree(e,f) {
+        return e+f;
+    },
+
+    render() {
+        return <div className='test'>
+            {this.functionone()}
+            {this.functiontwo()}
+            {this.functionthree()}
+        </div>;
+    }
+}
